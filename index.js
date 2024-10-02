dcc4dbe4001bcad8167884df4a9341c90b1f7a89
Athena <athenachang17@gmail.com>
2024-10-02 14:44:50 -0400

    finish function 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Code your solutions in this file
 
-// 1. fiveToOneHundred Function
+console.log("1. fiveToOneHundred Function");
 const fiveToOneHundred = () => {
     for (i = 5; i <= 100; i++) {
         console.log(i);
@@ -8,7 +8,7 @@ const fiveToOneHundred = () => {
 }
 fiveToOneHundred(5);
 
-// 2. console logs all positive numbers to 100 that is a multiple of 3
+console.log("2. console logs all positive numbers to 100 that is a multiple of 3");
 const multiplesOfThree = () =>{
     for (let i = 1; i <= 100; i++){
         if (i%3 === 0){
@@ -18,26 +18,28 @@ const multiplesOfThree = () =>{
 }
 multiplesOfThree();
 
-// 3. multiplesOfThreeOrFive Function
+console.log("3. multiplesOfThreeOrFive Function");
 const multiplesOfThreeOrFive = () => {
-    for (i = 0; i <= 100; i++) {
-        if (i % 3 === i % 5) {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
             console.log(i);
-        } else if (i % 3) {
-            console.log(i);
-        } else if (i % 5) {
-            console.log(i);
-        };
-        return;
+        } 
     };
 }
 
 multiplesOfThreeOrFive();
 
-// 4. `untilNum` takes in an integer parameter and console logs all numbers from 1 to that number.
+console.log("4. `untilNum` takes in an integer parameter and console logs all numbers from 1 to that number.");
 
-// 5. 
+console.log("5. multiply Function");
+const multiply = (a, b) => {
+    a * b = i;
+    console.log(i);
+};
 
+multiply(2, 4)     //returns 8
+multiply(10, -5)   //returns -50
+multiply(3, 7.5)   //returns 22.5
 
 // 6. 
 
