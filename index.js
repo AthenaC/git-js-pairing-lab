dc8543ca81b2ce4ff2f4ec9590cd1e62805b3e2e
Novis99 <ryanedmond82@gmail.com>
2024-10-02 14:02:33 -0400

    Function Two

--- index.js
+++ index.js
@@ -3,15 +3,27 @@
 // 1. fiveToOneHUndred Function
 
 
-// 2. 
-
-
+// 2. console logs all positive numbers to 100 that is a multiple of 3
+const multiplesOfThree = () =>{
+    for (let i = 1; i <= 100; i++){
+        if (i%3 === 0){
+            console.log(i)
+        }
+    }
+}
+multiplesOfThree();
 // 3. 
 
 
-// 4. 
-
-
+// 4. `untilNum` takes in an integer parameter and console logs all numbers from 1 to that number.
+const untilNum = (num) =>{
+    for (let i = 1; i <= num; i++){
+        console.log(i)
+    }
+}
+untilNum(5)    //prints out numbers 1 through 5
+untilNum(9)    //prints out numbers 1 through 9
+untilNum(42)   //prints out numbers 1 through 42
 // 5. 
 
 
