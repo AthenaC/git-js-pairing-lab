f43469e62e6897b88e5a6d5f26bb770e7caf4744
Novis99 <ryanedmond82@gmail.com>
2024-10-02 14:14:12 -0400

    Finished Func 2

--- index.js
+++ index.js
@@ -16,14 +16,7 @@ multiplesOfThree();
 
 
 // 4. `untilNum` takes in an integer parameter and console logs all numbers from 1 to that number.
-const untilNum = (num) =>{
-    for (let i = 1; i <= num; i++){
-        console.log(i)
-    }
-}
-untilNum(5)    //prints out numbers 1 through 5
-untilNum(9)    //prints out numbers 1 through 9
-untilNum(42)   //prints out numbers 1 through 42
+
 // 5. 
 
 
