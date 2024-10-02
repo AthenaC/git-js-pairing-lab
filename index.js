58657ccedbb5c7fc02a5bf1ce17c74862b513d6e
Athena <athenachang17@gmail.com>
2024-10-02 13:47:26 -0400

    added list

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 //Code your solutions in this file
+
+// 1. fiveToOneHUndred Function
+
+
+// 2. 
+
+
+// 3. 
+
+
+// 4. 
+
+
+// 5. 
+
+
+// 6. 
+
+
+// 7. 
+
+
+// 8. 
+
+
+// 9. 
+
+
+// 10. 
+
+
+
+
+
+
+
+
+
+
+
+
